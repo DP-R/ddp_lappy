8824a0d6085b890c6b73fa31b72226cdc0c7e59d
dp-r <purnendradatturi@gmail.com>
2022-10-20 12:14:05 +0530

    sep10 in main

--- main.py
+++ main.py
@@ -3,24 +3,19 @@ from init_matrix import walls,nr_agents, nr_experiments
   
 # from init_matrix import positionmatrix
 with open('init_matrix_data.pkl', 'rb') as file:    positionmatrix = pickle.load(file)
-for i in positionmatrix:
-    print(i)
+
 class Agent(object):
     def __init__(self,pos,mass,radius,dSpeed,dest):
-        self.pos=pos
-        self.mass=mass
-        self.radius=radius
-        self.dSpeed=dSpeed
-        self.dest=dest
+        (self.pos,self.mass,self.radius,self.dSpeed,self.dest)=(pos,mass,radius,dSpeed,dest)
 
         self.aVelocity=np.array([0,0])
         self.direction=normalize(self.dest-self.pos)
         self.dVelocity=self.dSpeed*self.direction
 
         self.acclTime=0.5
-        self.bodyFactor=2000
-        self.F=3000
-        self.delta=4
+        self.bodyFactor=120000
+        self.F=2000
+        self.delta=0.08*50
     def velocity_force(self):
         return (self.dVelocity-self.aVelocity)*self.mass/self.acclTime
     def f_ij(self,other):
@@ -47,29 +42,28 @@ def draw_walls():
         pygame.draw.line(roomscreen,line_color,([wall[0],wall[1]]),([wall[2],wall[3]]),3)
         pygame.display.flip()
 
-def position_update():
-    roomscreen.fill(background_color)
-    dt=0.05
+def position_update(pos_mat,timer):
+    # roomscreen.fill(background_color)
+    dt=0.1
     for agent_i in agents:
-        if agent_i.pos[0]>699 or agent_i.pos[0]<100:
+        if agent_i.pos[0]>19.99:
             agents.remove(agent_i)
 
         aVelocity_force=agent_i.velocity_force()
-        print(aVelocity_force)
         people_interaction=0;wall_interaction=0;
 
         for agent_j in agents:
             if agent_i == agent_j: continue
-            # print(agent_i.f_ij(agent_j))
             people_interaction+=agent_i.f_ij(agent_j)
         for wall in walls:
             wall_interaction+=agent_i.f_ik_wall(wall)
         # print(aVelocity_force,people_interaction,wall_interaction)
         agent_i.aVelocity=agent_i.aVelocity+(aVelocity_force+people_interaction+wall_interaction)*dt/agent_i.mass
         agent_i.pos+=agent_i.aVelocity*dt
-        # print(agent_i.pos,agent_i.aVelocity)
-        pygame.draw.circle(roomscreen, agent_color, agent_i.pos, round(agent_i.radius), 3)
-        pygame.draw.line(roomscreen, agent_color, agent_i.pos,agent_i.pos+10*agent_i.direction, 2)
+        timer+=dt
+        pos_mat.append([agent_i.pos,agent_i.mass,agent_i.radius,timer])
+        # pygame.draw.circle(roomscreen, agent_color, agent_i.pos, round(agent_i.radius), 3)
+        # pygame.draw.line(roomscreen, agent_color, agent_i.pos,agent_i.pos+10*agent_i.direction, 2)
 
 WHITE = (255,255,255);RED = (255,0,0);GREEN = (0,255,0);BLACK = (0,0,0)
 background_color = WHITE;agent_color = GREEN;line_color = BLACK
@@ -77,17 +71,18 @@ background_color = WHITE;agent_color = GREEN;line_color = BLACK
 roomscreen = pygame.display.set_mode((800,800))
 # roomscreen.fill(background_color)
 agents=agent_matrix()
-# pygame.display.update()
-from pygame_recorder import ScreenRecorder
-recorder = ScreenRecorder(800, 800, 60)
+pos_mat=[];timer=0;
+pygame.display.update()
+a=time.time()
+while True:
 
-
-for i in range(1):
-    # roomscreen.fill(background_color)
+    if len(agents)==1: 
+        print(time.time()-a)
+        break;
+    roomscreen.fill(background_color)
     draw_walls()
-    recorder.capture_frame(roomscreen)
-    position_update()
-    # print('done\n')
-recorder.end_recording()
+    position_update(pos_mat,timer)
+for i in pos_mat:
+    print(i[3])
 pygame.quit()
 os.system('spd-say "done"')
