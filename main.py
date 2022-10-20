607c0d0664d961fb7a179cf4a794863fe3f1f6d0
dp-r <purnendradatturi@gmail.com>
2022-10-20 12:15:11 +0530

    sep15 in main

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
+# -*- coding: utf-8 -*-
+
+def magnitude(vector):
+    return math.sqrt(sum(pow(element, 2) for element in vector))
 from init_func import *
 from init_matrix import walls,nr_agents, nr_experiments
   
 # from init_matrix import positionmatrix
 with open('init_matrix_data.pkl', 'rb') as file:    positionmatrix = pickle.load(file)
-
+pm=np.array(positionmatrix)
 class Agent(object):
     def __init__(self,pos,mass,radius,dSpeed,dest):
         (self.pos,self.mass,self.radius,self.dSpeed,self.dest)=(pos,mass,radius,dSpeed,dest)
@@ -14,10 +18,8 @@ class Agent(object):
         self.direction=normalize(self.dest-self.pos)
         self.dVelocity=self.dSpeed*self.direction
 
-        self.acclTime=0.5
-        self.bodyFactor=120000
-        self.F=2000
-        self.delta=0.08*50
+        (self.acclTime,self.bodyFactor,self.F,self.delta)=(0.5,120000,2000,0.08*50)
+
     def velocity_force(self):
         return (self.dVelocity-self.aVelocity)*self.mass/self.acclTime
     def f_ij(self,other):
@@ -30,6 +32,19 @@ class Agent(object):
         d_iw,e_iw = distance_agent_to_wall(self.pos,wall)
         return -self.F*np.exp((r_i-d_iw)/self.delta)*e_iw+self.bodyFactor*g(r_i-d_iw)*e_iw
 
+r_i=pm[:,[2]]
+d_i=pm[:,[0]]
+ab=np.array([0,0])
+(acclTime,bodyFactor,F,delta)=(0.5,120000,2000,0.08*50)
+for j in range(len(r_i)):
+    r_ij=r_i+r_i[j]
+    d_ij=d_i-d_i[j]
+    e_ij=np.divide(d_ij.T,np.array([magnitude(np.linalg.norm(x)) for x in d_ij]))
+    d_ij=np.array([magnitude(np.linalg.norm(x)) for x in d_ij])
+    d_ij=d_ij.reshape([nr_agents,1])
+    ab=(F*np.exp((r_ij.astype(float)-d_ij)/(delta))*e_ij+bodyFactor*g(r_ij.astype(float)-d_ij)*e_ij)
+    
+    
 def agent_matrix():
     agents=[]
     for i in range(nr_agents):
@@ -64,25 +79,25 @@ def position_update(pos_mat,timer):
         agent_i.pos+=agent_i.aVelocity*dt
         timer+=dt
         pos_mat.append([agent_i.pos,agent_i.mass,agent_i.radius,timer])
-        pygame.draw.circle(roomscreen, agent_color, agent_i.pos, round(agent_i.radius), 3)
-        pygame.draw.line(roomscreen, agent_color, agent_i.pos,agent_i.pos+10*agent_i.direction, 2)
+        # pygame.draw.circle(roomscreen, agent_color, agent_i.pos, round(agent_i.radius), 3)
+        # pygame.draw.line(roomscreen, agent_color, agent_i.pos,agent_i.pos+10*agent_i.direction, 2)
 
 WHITE = (255,255,255);RED = (255,0,0);GREEN = (0,255,0);BLACK = (0,0,0)
 background_color = WHITE;agent_color = GREEN;line_color = BLACK
 
-roomscreen = pygame.display.set_mode((800,800))
-roomscreen.fill(background_color)
+# roomscreen = pygame.display.set_mode((800,800))
+# roomscreen.fill(background_color)
 agents=agent_matrix()
 pos_mat=[];timer=0;
-pygame.display.update()
+# pygame.display.update()
 a=time.time()
 # while True:
-for i in range(100):
+for i in range(1):
     if len(agents)==1: 
         print(time.time()-a)
         break;
-    roomscreen.fill(background_color)
-    draw_walls()
+    # roomscreen.fill(background_color)
+    # draw_walls()
     position_update(pos_mat,timer)
 for i in pos_mat:
     print(i)
