b4fd1351fd305382a742d617728d1a933bfc93a1
dp-r <purnendradatturi@gmail.com>
2022-11-16 18:16:29 +0530

    new modifications

--- pos.m
+++ pos.m
@@ -11,7 +11,7 @@ time=0;
 am=pm; am(:,[5,6,7,8])=0;
 am(:,9)=1;
 finalmat=am;
-for i=1:10000
+for i=1:1000
 
     swf=zeros(nr_agents,2);
     spf=zeros(nr_agents,2);
@@ -35,8 +35,8 @@ end
     wf=swf;
 for i=1:size(walls)
     [d_iw,e_iw]=test_walldistance(am(:,[1,2]),walls(i,:));
-    wf=wf-10*(+F.*exp((r_i-d_iw)/(delta)).*e_iw+bodyFactor.*max(r_i-d_iw,0).*e_iw);
-
+    wf=wf+1*(+F.*exp((r_i-d_iw)/(delta)).*e_iw+bodyFactor.*max(r_i-d_iw,0).*e_iw);
+	wf
 end
 
 % [d_iw,e_iw]=test_walldistance(am(:,[1,2]),negativewall);
@@ -44,13 +44,14 @@ end
 % wf=wf-(+F.*exp((r_i-d_iw)/(delta)).*e_iw+bodyFactor.*max(r_i-d_iw,0).*e_iw);
 
     wf=wf.*am(:,9);
-    swf=wf;    
+    swf=wf;   
 
     dt=0.008;
     direc=znorm(pm(:,[6,7])-am(:,[1,2]));
 
     af=(pm(:,5).*direc-am(:,[5,6])).*pm(:,3)/acclTime;
     af=af.*am(:,9);
+	swf;
     am(:,[5,6])=am(:,[5,6])+(af+spf+swf)./pm(:,3)*dt;
     am(:,[1,2])=am(:,[1,2])+am(:,[5,6]).*dt;
     time=time+dt;
@@ -64,6 +65,7 @@ end
 end
 
 save('finalmat.mat','finalmat')
+
 function [dist,npw]=test_walldistance(point, wall)
     p0=wall(1,[1,2]);
     p1=wall(1,[3,4]);
@@ -88,7 +90,9 @@ function [dist,npw]=test_walldistance(point, wall)
     end
     a=cross-point;
     npw = -a./zmod(a);
-% doubtful on direction of normal force from the wall when crowd is away from the edge
+%     disp(dist)
+%    disp(npw)
+    % doubtful on direction of normal force from the wall when crowd is away from the edge
 end
 
 function zdot=zdot(a,b)
